60d6437293cab67897418f89c4b97a912202ad23
vladimirterentjev <vt@terraquantum.swiss>
2025-07-07 19:46:03 +0200

    feat: embedding model retry with tenacity

--- rag_battle/infra/embeddings/tei/model.py
+++ rag_battle/infra/embeddings/tei/model.py
@@ -1,5 +1,7 @@
 import time
 
+import tenacity
+from grpc import RpcError
 from loguru import logger
 from rag_battle.domain.embeddings_model import (
     BaseEmbeddingsModel,
@@ -23,6 +25,27 @@ class TEIEmbeddingsModel(BaseEmbeddingsModel[TEIEmbeddingsModelConfig]):
     async def embed_documents(self, texts: list[str]) -> list[list[float]]:
         return await self._embed(texts)
 
+    @staticmethod
+    def is_retryable_grpc_error(exception: BaseException) -> bool:
+        if isinstance(exception, RpcError):
+            return exception.code().name in {
+                "UNAVAILABLE",
+                "DEADLINE_EXCEEDED",
+                "RESOURCE_EXHAUSTED",
+            }
+        return False
+
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(5),
+        wait=(
+            # use exponential backoff to gradually increase delay between retries
+            tenacity.wait_exponential(multiplier=0.5, min=0.1, max=1.0)
+            # add jitter to prevent retry storm when many clients retry simultaneously
+            + tenacity.wait_random(0, 1)
+        ),
+        retry=tenacity.retry_if_exception(is_retryable_grpc_error),
+        reraise=True,
+    )
     async def _embed(self, texts: list[str]) -> list[list[float]]:
         start_time = time.time()
         embeddings = await embeddings_grpc(
